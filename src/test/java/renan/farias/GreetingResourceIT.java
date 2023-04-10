9f7428417c2d7de9d75a48e358fcf4669305f3b5
Renan <renan@pop-os.localdomain>
2023-04-10 15:48:19 +0100

    configuring project

---
+++ src/test/java/renan/farias/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package renan.farias;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
